9397940f67b35ee4e76460954da998548419d27a
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 17:54:10 -0700

    Update index.php

--- index.php
+++ index.php
@@ -306,7 +306,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 								<a href="services.html">Services</a> |
 								<a href="price.html">Price</a> |
 								<a href="projects.html">Projects</a> |
-								<a href="contact.php">Contact</a>
+								<a href="contact.php">ContactRohit</a>
                                 <a href="users.php">Users</a>
 							</p>
 						</div>
