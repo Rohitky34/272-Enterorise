7ca8023b3270caf112a4812709ce28a528505c2a
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 21:22:20 -0700

    Update users.php

--- users.php
+++ users.php
@@ -18,4 +18,29 @@
             <td align="left"><input type="submit" value="Login" name="submitBtn"/>
             <input type="reset" value="Reset" name="reset"/></td>
 </form>
+<?php
 
+$userN = $_POST['user-name'];
+$passW = $_POST['pass-word'];
+$userlist = file ('users.txt');
+
+$email = "";
+$company = "";
+
+$success = false;
+foreach ($userlist as $user) {
+    $user_details = explode('|', $user);
+    if ($user_details[0] == $userN && $user_details[1] == $passW) {
+        $success = true;
+        $email = $user_details[2];
+        $company = $user_details[3];
+        break;
+    }
+}
+
+if ($success) {
+    echo "<br> Hi $userN you have been logged in. <br>";
+} else {
+    echo "<br> You have entered the wrong username or password. Please try again. <br>";
+}
+?>
