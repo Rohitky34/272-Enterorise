7327827fa6dda235b782be1308b09a95dd700c9d
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 21:52:03 -0700

    Update users.php

--- users.php
+++ users.php
@@ -1,28 +1,26 @@
-
-
-<?php 
-    $myFile = "users.txt";
-    $contents = file_get_contents($myFile);
-    $contents = explode("\n", $contents);
-
-foreach($contents as $values){
-     $loginInfo = explode(":", $values);
-        $user = $loginInfo[0];
-        $password = $loginInfo[1];
-
-    if($user == $_POST['username'] && $password == $_POST['password']){
-        session_start(); 
-        header('Location: users.php');
-   }
-    else{
-        echo '<script>alert("Please verify your username and password.");</script>'
-    }
-}
-?>
-
-<form action=" " method="post">
-   <h2>Username:</h2><input type="text" id="username"><br>
-   <h2>Password:</h2><input type="password" id="password"><br>
-   <input type="submit" value ="submit">
-    <input type="reset" value ="reset">
+<html>
+<head>
+<title> Login Page   </title>
+</head>
+<body>
+<form action="" method="post">
+    <table width="200" border="0">
+  <tr>
+    <td>  UserName</td>
+    <td> <input type="text" name="user" > </td>
+  </tr>
+  <tr>
+    <td> PassWord  </td>
+    <td><input type="password" name="pass"></td>
+  </tr>
+  <tr>
+    <td> <input type="submit" name="login" value="LOGIN"></td>
+    <td></td>
+  </tr>
+</table>
 </form>
+</body>
+</html>
+
+
+
