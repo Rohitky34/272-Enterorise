e785a07449facb462c0876e1ce6c34cad9697472
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 18:12:01 -0700

    Update users.php

--- users.php
+++ users.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
 <form method="post" action="login.php">
     <table border="0" cellpadding="0">
         <tr>
@@ -22,26 +19,3 @@
             <input type="reset" value="Reset" name="reset"/></td>
 </form>
 
-<!-- <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if (isset($_POST['submitBtn'])){
-$file = explode( "PHP_EOL", file_get_contents( "users.txt" ));
-foreach( $file as $line ) {
-list($username,$password) == explode( "||", $line );
-
-if($_POST['username'] = $username && $_POST['password']) {
-// User authenticated correctly
-echo "You've successfully logged in!";
-echo "<br>";
-} else {
-// User did not authenticate
-echo "Invalid Username or Password";
-}
-}
-} 
-?> -->
-
-</head>
-</html>
