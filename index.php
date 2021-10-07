4de983a5fc71eb92483eae9fa621ea2086d2530c
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 17:26:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -32,12 +32,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 			<div class="navbar-header">
 				<!-- Button for smallest screens -->
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-				<a class="navbar-brand" href="index.html">
+				<a class="navbar-brand" href="index.php">
 					<img src="assets/images/logo.png" alt="Techro HTML5 template"></a>
 			</div>
 			<div class="navbar-collapse collapse">
 				<ul class="nav navbar-nav pull-right mainNav">
-					<li class="active"><a href="index.html">Home</a></li>
+					<li class="active"><a href="index.php">Home</a></li>
 					<li><a href="about.html">About</a></li>
 					<li><a href="services.html">Services</a></li>
 					<li><a href="price.html">Price</a></li>
@@ -51,7 +51,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 							<li><a href="#">Dummy Link3</a></li>
 						</ul>
 					</li>
-					<li><a href="contact.html">Contact</a></li>
+					<li><a href="contact.php">Contact</a></li>
 
 				</ul>
 			</div>
@@ -301,7 +301,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 					<div class="col-md-6 panel">
 						<div class="panel-body">
 							<p class="simplenav">
-								<a href="index.html">Home</a> | 
+								<a href="index.php">Home</a> | 
 								<a href="about.html">About</a> |
 								<a href="services.html">Services</a> |
 								<a href="price.html">Price</a> |
