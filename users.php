eff3a0c2c14a7730390bb123fd836c066bb1d09f
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 22:02:27 -0700

    Update users.php

--- users.php
+++ users.php
@@ -1,45 +1,38 @@
-<html>
-<head>
-<title> Login Page   </title>
-</head>
-<body>
-<form action="" method="post">
-    <table width="200" border="0">
-  <tr>
-    <td>  UserName</td>
-    <td> <input type="text" name="user" > </td>
-  </tr>
-  <tr>
-    <td> PassWord  </td>
-    <td><input type="password" name="pass"></td>
-  </tr>
-  <tr>
-    <td> <input type="submit" name="login" value="LOGIN"></td>
-    <td></td>
-  </tr>
-</table>
-</form>
-</body>
-</html>
-
-<?php 
-    $myFile = "users.txt";
-    $contents = file_get_contents($myFile);
-    $contents = explode("\n", $contents);
-
-foreach($contents as $values){
-     $loginInfo = explode(":", $values);
-        $user = $loginInfo[0];
-        $password = $loginInfo[1];
-
-    if($user == $_POST['username'] && $password == $_POST['password']){
-        session_start(); 
-        header('Location: login.php');
-   }
-    else{
-        echo '<script>alert("Please verify your username and password.");</script>'
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+<div class="wrapper" style="width: 30%; margin: 0 auto;">
+    <form class="form-signin" action='#' method="post">       
+      <h2 class="form-signin-heading">User Login</h2><br/>
+      <input type="text" class="form-control" name="username" placeholder="username" required="" autofocus="" /><br/>
+      <input type="password" class="form-control" name="password" placeholder="Password" required=""/><br/>
+      <button class="btn btn-small btn-primary btn-block" type="submit">Login</button>  
+    </form>
+    <?php 
+    function check_password($username, $password){
+        $pwd_file = 'users.txt';
+        if(!$fh = fopen($pwd_file, "r")) {die("<p>Could not open password file");}
+        $match = 0;
+        $pwd = md5($password);
+        while(!feof($fh)) {
+          $line = fgets($fh, 4096);
+          $user_pass = explode(":", $line);
+          if($user_pass[0] == $username) {
+            if(rtrim($user_pass[1]) == $pwd) {
+              $match = 1;
+              break;
+            }
+          }
+          $match = 2; 
+        }
+        if($match == '1') {
+           echo "<b>Login Success!</b>";
+        } 
+        if($match == '2') {
+           echo "<b>Login Failed!</b>";
+        } 
+        fclose($fh);
     }
-}
+    if($_POST['username']) {
+        check_password($_POST['username'], $_POST['password']);
+    }
     ?>
-
-
+</div>  
