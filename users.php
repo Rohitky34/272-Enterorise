479b9e92d28c882684ebd069cfb0906d1a97e70d
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-06 21:53:21 -0700

    Update users.php

--- users.php
+++ users.php
@@ -22,5 +22,24 @@
 </body>
 </html>
 
+<?php 
+    $myFile = "users.txt";
+    $contents = file_get_contents($myFile);
+    $contents = explode("\n", $contents);
+
+foreach($contents as $values){
+     $loginInfo = explode(":", $values);
+        $user = $loginInfo[0];
+        $password = $loginInfo[1];
+
+    if($user == $_POST['username'] && $password == $_POST['password']){
+        session_start(); 
+        header('Location: login.php');
+   }
+    else{
+        echo '<script>alert("Please verify your username and password.");</script>'
+    }
+}
+    ?>
 
 
