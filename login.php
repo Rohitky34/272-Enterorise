65452b76a4959771f7d62f71ce2e0378db7cfef1
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-07 17:24:30 -0700

    as

--- login.php
+++ login.php
@@ -1,10 +1,37 @@
-<?php
-$file = fopen("users.txt","r");
+    <?php 
+    function check_password($username, $password){
+        $pwd_file = 'userAdmin.txt';
+        if(!$fh = fopen($pwd_file, "r")) {die("<p>Could not open password file");}
+        $match = 0;
+        $pwd = $password;
+        while(!feof($fh)) {
+          $line = fgets($fh, 4096);
+          $user_pass = explode(":", $line);
+          if($user_pass[0] == $username) {
+            if(rtrim($user_pass[1]) == rtrim($pwd)) {
+              $match = 1;
+              break;
+            }
+          }
+          $match = 2; 
+        }
+        if($match == '1') {
+           $file = fopen("users.txt","r");
 
-while(! feof($file))
-{
-echo fgets($file). "<br />";
-}
+            while(! feof($file))
+            {
+            echo fgets($file). "<br />";
+            }
 
 fclose($file);
-?>
+        } 
+        if($match == '2') {
+           echo "<b>Login Failed!</b>";
+        } 
+        fclose($fh);
+    }
+    if($_POST['username']) {
+        check_password($_POST['username'], $_POST['password']);
+    }
+    ?>
+
--- users.php
+++ users.php
@@ -7,32 +7,4 @@
       <button class="btn btn-small btn-primary btn-block" type="submit">Login</button>  
     </form>
 </div>
-    <?php 
-    function check_password($username, $password){
-        $pwd_file = 'userAdmin.txt';
-        if(!$fh = fopen($pwd_file, "r")) {die("<p>Could not open password file");}
-        $match = 0;
-        $pwd = $password;
-        while(!feof($fh)) {
-          $line = fgets($fh, 4096);
-          $user_pass = explode(":", $line);
-          if($user_pass[0] == $username) {
-            if(rtrim($user_pass[1]) == rtrim($pwd)) {
-              $match = 1;
-              break;
-            }
-          }
-          $match = 2; 
-        }
-        if($match == '1') {
-           header("Location: login.php");
-        } 
-        if($match == '2') {
-           echo "<b>Login Failed!</b>";
-        } 
-        fclose($fh);
-    }
-    if($_POST['username']) {
-        check_password($_POST['username'], $_POST['password']);
-    }
-    ?>
+
