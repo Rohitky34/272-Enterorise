0afd4f64e6ee381d5f952c48f73bf45dfe1cf82e
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-10-07 17:09:56 -0700

    valid

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
-
+<h2> Welcome </h2>
 
 <?php
-<h2> Welcome $_POST['username']</h2>
 if($_POST['username']){
     echo $_POST['username'];
 }
--- users.php
+++ users.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 <div class="wrapper" style="width: 30%; margin: 0 auto;">
-    <form class="form-signin" action='#' method="post">       
+    <form class="form-signin" action='login.php' method="post">       
       <h2 class="form-signin-heading">User Login</h2><br/>
       <input type="text" class="form-control" name="username" placeholder="username" required="" autofocus="" /><br/>
       <input type="password" class="form-control" name="password" placeholder="Password" required=""/><br/>
