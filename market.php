fde3c7d2ef3a362130f66b6d4d7027f05bb6d577
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-10 15:09:46 -0800

    Update market.php

--- market.php
+++ market.php
@@ -2,4 +2,37 @@
 <html lang="en">
 <head>
 <h1>Welcome to market place</h1>
+
+<div class = "RealEstate">
+    <button id="myButton1" class="float-left submit-button" style="background:; cursor:pointer">Real Estate Services</button>
+    <script type="text/javascript">
+        document.getElementById("myButton1").onclick = function () {
+            location.href = "rohitky.me";
+        };
+    </script>
+</div>
+<div class = "School">
+    <button id="myButton2" class="float-left submit-button" style="background:; cursor:pointer">Real Estate Services</button>
+    <script type="text/javascript">
+        document.getElementById("myButton2").onclick = function () {
+            location.href = "rhttp://thisisjay.me"
+        };
+    </script>
+</div>
+<div class = "Cricket Shop">
+    <button id="myButton3" class="float-left submit-button" style="background:; cursor:pointer">Real Estate Services</button>
+    <script type="text/javascript">
+        document.getElementById("myButton3").onclick = function () {
+            location.href = "http://yamsanisaisanthosh.live/home.html#";
+        };
+    </script>
+</div>
+<div class = "Spartan Tours & Travels ">
+    <button id="myButton4" class="float-left submit-button" style="background:; cursor:pointer">Real Estate Services</button>
+    <script type="text/javascript">
+        document.getElementById("myButton4").onclick = function () {
+            location.href = "https://afrozinamdar.me/";
+        };
+    </script>
+</div>
 </head>
