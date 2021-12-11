b634adc0dada20f8a4491247b407828a5371056e
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-10 16:38:00 -0800

    Update market.php

--- market.php
+++ market.php
@@ -68,13 +68,10 @@ span.psw {
 </head>
 <body>
 
-<h2>Login Form</h2>
+<h2>Welcome to Market Place</h2>
+<h2>Log In</h2>
 
 <form action="tracking.php" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
-
   <div class="container">
     <label for="uname"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
@@ -90,7 +87,6 @@ span.psw {
 
   <div class="container" style="background-color:#f1f1f1">
     <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
 
