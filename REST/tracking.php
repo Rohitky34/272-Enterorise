d84649a63d0232c437c4def6e9cd67c217c98536
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-11 13:48:58 -0800

    Update tracking.php

--- REST/tracking.php
+++ REST/tracking.php
@@ -15,8 +15,9 @@ $data= json_decode(file_get_contents("php://input"));
 $productName = $data->productName;
 $companyName = $data->companyName;
 $email = $data->email;
+$visited = $data->visited;
 
-$sql = "INSERT INTO Tracking (email, product_name, company_name) VALUES ('$email','$productName', '$companyName')";
+$sql = "INSERT INTO Tracking (email, product_name, company_name, visited) VALUES ('$email','$productName', '$companyName', '$visited')";
 
 if ($conn->query($sql) === TRUE) {
     echo "True";
