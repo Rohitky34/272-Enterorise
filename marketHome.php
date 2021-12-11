33dda74a89588f6c8a4401e727627304b3e58dbc
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-11 13:13:30 -0800

    Update marketHome.php

--- marketHome.php
+++ marketHome.php
@@ -13,6 +13,7 @@
    header('location:signedIn.php');
     }
     
+    
 ?>
 </head>
 
@@ -26,7 +27,7 @@
         </script>
     </div>
     <div class = "School">
-        <a href="http://thisisjay.me/" onClick= 'setcookie("email", $email, time() + 2 * 24 * 60 * 60)'>School</a>
+        <a href="http://thisisjay.me/" onClick= 'setcookie("email", $_COOKIE["email"], time() + 2 * 24 * 60 * 60)'>School</a>
         <!-- <button id="myButton2" class="float-left submit-button" style="background:; cursor:pointer">School Services</button> -->
         <!-- <script type="text/javascript">
             document.getElementById("myButton2").onclick = function () {
