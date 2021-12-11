b8749994f80a689067923e4c9271c7bca281b9db
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-10 16:32:34 -0800

    Update market.php

--- market.php
+++ market.php
@@ -1,38 +1,98 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<h1>Welcome to market place</h1>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+form {border: 3px solid #f1f1f1;}
 
-<div class = "RealEstate">
-    <button id="myButton1" class="float-left submit-button" style="background:; cursor:pointer">Real Estate Services</button>
-    <script type="text/javascript">
-        document.getElementById("myButton1").onclick = function () {
-            location.href = "http://rohitky.me/";
-        };
-    </script>
-</div>
-<div class = "School">
-    <button id="myButton2" class="float-left submit-button" style="background:; cursor:pointer">School Services</button>
-    <script type="text/javascript">
-        document.getElementById("myButton2").onclick = function () {
-            location.href = "http://thisisjay.me/";
-        };
-    </script>
-</div>
-<div class = "Cricket Shop">
-    <button id="myButton3" class="float-left submit-button" style="background:; cursor:pointer">Cricket Shop</button>
-    <script type="text/javascript">
-        document.getElementById("myButton3").onclick = function () {
-            location.href = "http://yamsanisaisanthosh.live/home.html#";
-        };
-    </script>
-</div>
-<div class = "Spartan Tours & Travels ">
-    <button id="myButton4" class="float-left submit-button" style="background:; cursor:pointer">Spartan Tours & Travels</button>
-    <script type="text/javascript">
-        document.getElementById("myButton4").onclick = function () {
-            location.href = "http://afrozinamdar.me/";
-        };
-    </script>
-</div>
+input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+}
+
+button:hover {
+  opacity: 0.8;
+}
+
+.cancelbtn {
+  width: auto;
+  padding: 10px 18px;
+  background-color: #f44336;
+}
+
+.imgcontainer {
+  text-align: center;
+  margin: 24px 0 12px 0;
+}
+
+img.avatar {
+  width: 40%;
+  border-radius: 50%;
+}
+
+.container {
+  padding: 16px;
+}
+
+span.psw {
+  float: right;
+  padding-top: 16px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+  .cancelbtn {
+     width: 100%;
+  }
+}
+</style>
 </head>
+<body>
+
+<h2>Login Form</h2>
+
+<form action="tracking.php" method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+        
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+
+</body>
+</html>
