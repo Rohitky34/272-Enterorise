8efeb8709dfa9898ecff7dc656a86e4ad68ee738
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-11 16:37:15 -0800

    Update land.php

--- land.php
+++ land.php
@@ -20,22 +20,5 @@
 <?php 
 $path = "land";
 print_r($_COOKIE);
-if (!isset($_COOKIE[$path]['count']))
-{
-    ?>
-    Welcome! This is the first time you have viewed this page. 
-<?php
-    $cookie = 1;
-    setcookie($path."[count]", $cookie);
-}
-else
-{
-    $cookie = ++$_COOKIE[$path]['count'];
-    // $path = "courses";
-    setcookie($path."[count]", $cookie);
-    // setcookie($_COOKIE['path']['count'], $cookie);
-    ?>  
-    You have viewed this page <?= $_COOKIE[$path]['count'] ?> times. 
-<?php
-    }
+
 ?>
