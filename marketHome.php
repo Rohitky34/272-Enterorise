4363cd862484098a6c7f999c9b9322fcb6ddf02e
Rohit Yadav <rohitkumar.yadav@sjsu.edu>
2021-12-11 18:57:15 -0800

    Update marketHome.php

--- marketHome.php
+++ marketHome.php
@@ -110,6 +110,19 @@
             };
         </script>
     </div>
+    <?php
+        $ch =curl_init('http://thisisjay.me/productList.php');
+
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        $result = curl_exec($ch);
+
+        //  echo $result;  
+
+        echo $result;
+
+        // $res =json_decode($result);
+?>
 
 </body>
 </html>
